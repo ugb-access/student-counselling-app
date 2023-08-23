803f9cbb73e31e5e761f7261054efc3258c9a69a
Usman <usmanghanibaig2@gmail.com>
2023-08-23 14:57:08 +0500

    production domain added in api config

--- resources/js/config/api.js
+++ resources/js/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: import.meta.env.PROD
-        ? "http://13.42.226.161/api"
+        ? "http://royalvisionedu.com/api"
         : "http://localhost:8000/api",
     headers: {
         "Content-Type": "application/json",
